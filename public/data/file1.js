da7d0aba06e41f5d5c03d80e23952f9bb49f41ab
Ayush <ayush.hakmn@gmail.com>
2019-02-25 10:35:17 +0530

    added data files

---
+++ public/data/file1.js
@@ -0,0 +1,3 @@
+var file1= [{ 'Hao Wang': 'Martin Abadi', 'Stal Aanderaa': 'Hao Wang' },
+{ 'Hao Wang': 'ayush', 'Stal Aanderaa': 'Lars Aarvik' },
+{ 'Hao Wang': 'James Abbott', 'Stal Aanderaa': 'Zohar Manna' }]
