9422883d97fea580da561aab04dfe3d3caa4e9fd
mehtadhiraj <mehtadhiraj21@gmail.com>
2019-04-30 17:47:45 +0530

    Minoer changes

---
+++ public/data/facebookData.js
@@ -0,0 +1,4 @@
+var facebookData= [{ username: 'mehtadhiraj21@gmail.com-789456123',
+  friends: 'mehtamukul454@gmail.com-123456789' },
+{ username: 'mehtadhiraj21@gmail.com-789456123',
+  friends: 'karthikmudalair13@gmail.com-456789123' }]
